6930b03ff7a659a4946c229ae091f3237a6d2bcb
abhishek joshi <abhishek joshi>
2016-10-27 19:27:45 +0530

    first commit

---
+++ game display.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+width=600
+height=600
+gameDisplay = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Tic Tac Toe')
+white=[255,255,255]
+black=[0,0,0]
+red=[255,0,0]
+blue=[0,0,255]
